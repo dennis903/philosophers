4b9aa4ba61c30916da25b339efa0dc82c1003c9e
dennis903 <hyeongjin3@hanmail.net>
2021-06-11 16:42:24 +0900

    start

---
+++ include/philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeolee <hyeolee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/11 16:38:43 by hyeolee           #+#    #+#             */
+/*   Updated: 2021/06/11 16:42:04 by hyeolee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+#include <string.h> //for memset
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+#endif
